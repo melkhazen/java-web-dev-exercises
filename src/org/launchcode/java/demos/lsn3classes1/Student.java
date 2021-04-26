87201f58ab11105327cfc6ad76c8cfc6c02163be
mohamed <elkhazen1@gmail.com>
2021-04-26 17:42:35 -0500

    finished studio

--- src/org/launchcode/java/demos/lsn3classes1/Student.java
+++ src/org/launchcode/java/demos/lsn3classes1/Student.java
@@ -10,4 +10,36 @@ public class Student {
     private int numberOfCredits = 0;
     private double gpa = 0.0;
 
+    public Student(String aName, int aStudentId, int aNumberOfCredits, double aGpa) {
+        this.name = aName;
+        this.studentId = aStudentId;
+        this.numberOfCredits = aNumberOfCredits;
+        this.gpa = aGpa;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public int getStudentId(){
+        return studentId;
+    }
+    public int getNumberOfCredits() {
+        return numberOfCredits;
+    }
+    public double getGpa() {
+        return gpa;
+    }
+    public void setName(String aName) {
+        name = aName;
+    }
+    public void setStudentId(int aStudentId) {
+        studentId = aStudentId;
+    }
+    private void setNumberOfCredits(int aNumberOfCredits) {
+        this.numberOfCredits = aNumberOfCredits;
+    }
+    private void setGpa(double aGpa) {
+        gpa = aGpa;
+    }
+
 }
